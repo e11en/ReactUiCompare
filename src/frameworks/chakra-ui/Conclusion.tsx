e355abe7d1803257668959e3a5e27e1795bdc6ad
Ellen Langelaar <ellen.langelaar@nedap.com>
2021-04-13 10:31:15 +0200

    Added ChakraUI layout page

--- src/frameworks/chakra-ui/Conclusion.tsx
+++ src/frameworks/chakra-ui/Conclusion.tsx
@@ -25,14 +25,18 @@ export const Conclusion = () => {
       </ElementCard>
 
       <ElementCard halfwidth title="Future">
-        <p>They are actively maintaining and expanding the kit.</p>
+        <p>
+          This kit is really new and they are actively maintaining and expanding
+          the kit making some real progress.
+        </p>
       </ElementCard>
 
       <ElementCard halfwidth title="Community">
         <p>
           They acknowledge that the community is still small but in my own
           experience it pops up on forums every now and again of people
-          switching to this kit.
+          switching to this kit. Looking at the downloads per day chart you can
+          see they have had a really stead trend upwards.
         </p>
       </ElementCard>
 
@@ -41,11 +45,11 @@ export const Conclusion = () => {
           <TableBody>
             <TableRow>
               <TableCell>Package size (minzipped)</TableCell>
-              <TableCell>0 KB</TableCell>
+              <TableCell>104.6 KB</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Downloads per day</TableCell>
-              <TableCell>0</TableCell>
+              <TableCell>77.809</TableCell>
             </TableRow>
           </TableBody>
         </Table>
--- src/frameworks/chakra-ui/Layout.tsx
+++ src/frameworks/chakra-ui/Layout.tsx
@@ -1,14 +1,42 @@
+import {
+  Grid,
+  GridItem,
+  Container,
+  Box,
+  Center,
+  SimpleGrid,
+  VStack,
+  AspectRatio,
+} from "@chakra-ui/react";
+import styled from "styled-components";
+
 import Code from "components/CodeComponent";
-import ElementCard from "components/ElementCardComponent";
+import ElementCard, { SpacedChildren } from "components/ElementCardComponent";
 import FlexWrapper from "components/FlexWrapperComponent";
+
+const ColorContainer = styled(Container)`
+  background-color: #cfe8fc;
+`;
 
 const Containers = () => {
   return (
     <ElementCard fullwidth title="Container">
-      <div></div>
+      <div>
+        <VStack>
+          <ColorContainer maxW="container.xl">XL</ColorContainer>
+          <ColorContainer maxW="container.lg">L</ColorContainer>
+          <ColorContainer maxW="container.md">M</ColorContainer>
+          <ColorContainer maxW="container.sm" centerContent>
+            S
+          </ColorContainer>
+        </VStack>
+      </div>
       <Code>
         {`
-
+          <Container maxW="container.xl">XL</Container>
+          <Container maxW="container.lg">L</Container>
+          <Container maxW="container.md">M</Container>
+          <Container maxW="container.sm" centerContent>S</Container>
         `}
       </Code>
     </ElementCard>
@@ -18,10 +46,48 @@ const Containers = () => {
 const Grids = () => {
   return (
     <ElementCard title="Grid">
-      <div></div>
+      <SpacedChildren>
+        <Grid
+          h="200px"
+          templateRows="repeat(2, 1fr)"
+          templateColumns="repeat(5, 1fr)"
+          gap={4}
+        >
+          <GridItem rowSpan={2} colSpan={1} bg="#d3e2ef" />
+          <GridItem colSpan={2} bg="#d3e2ef" />
+          <GridItem colSpan={2} bg="#d3e2ef" />
+          <GridItem colSpan={4} bg="#d3e2ef" />
+        </Grid>
+
+        <SimpleGrid columns={2} spacing={1}>
+          <Box bg="#cfe8fc" height="80px" />
+          <Box bg="#cfe8fc" height="80px" />
+          <Box bg="#cfe8fc" height="80px" />
+          <Box bg="#cfe8fc" height="80px" />
+          <Box bg="#cfe8fc" height="80px" />
+        </SimpleGrid>
+      </SpacedChildren>
       <Code>
         {`
+          <Grid
+            h="200px"
+            templateRows="repeat(2, 1fr)"
+            templateColumns="repeat(5, 1fr)"
+            gap={4}
+          >
+            <GridItem rowSpan={2} colSpan={1} bg="#d3e2ef" />
+            <GridItem colSpan={2} bg="#d3e2ef" />
+            <GridItem colSpan={2} bg="#d3e2ef" />
+            <GridItem colSpan={4} bg="#d3e2ef" />
+          </Grid>
 
+          <SimpleGrid columns={2} spacing={1} m={2}>
+            <Box bg="#cfe8fc" height="80px" />
+            <Box bg="#cfe8fc" height="80px" />
+            <Box bg="#cfe8fc" height="80px" />
+            <Box bg="#cfe8fc" height="80px" />
+            <Box bg="#cfe8fc" height="80px" />
+          </SimpleGrid>
         `}
       </Code>
     </ElementCard>
@@ -31,10 +97,21 @@ const Grids = () => {
 const Other = () => {
   return (
     <ElementCard title="Other">
-      <div></div>
+      <SpacedChildren>
+        <Center bg="#d3e2ef" h="100px">
+          Center
+        </Center>
+        <AspectRatio maxW="100px" ratio={1}>
+          <Box bg="#cfe8fc">Aspect ratio</Box>
+        </AspectRatio>
+      </SpacedChildren>
       <Code>
         {`
-        
+          <Center bg="#d3e2ef" h="100px">Center</Center>
+
+          <AspectRatio maxW="100px" ratio={1}>
+            <Box bg="#cfe8fc">Aspect ratio</Box>
+          </AspectRatio>
         `}
       </Code>
     </ElementCard>
