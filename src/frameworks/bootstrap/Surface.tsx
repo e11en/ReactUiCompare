dad51f972daea37a9116fc2486e24b44586d5a71
Ellen Langelaar <ellen.langelaar@nedap.com>
2021-04-13 17:23:46 +0200

    Added Bootstrap surface

--- src/frameworks/bootstrap/Surface.tsx
+++ src/frameworks/bootstrap/Surface.tsx
@@ -1,27 +1,45 @@
+import { Card, Accordion, Navbar } from "react-bootstrap";
+import styled from "styled-components";
+
 import Code from "components/CodeComponent";
-import ElementCard from "components/ElementCardComponent";
+import ElementCard, { SpacedChildren } from "components/ElementCardComponent";
 import FlexWrapper from "components/FlexWrapperComponent";
+
+const SmallCard = styled(Card)`
+  width: 286px;
+
+  img {
+    height: 180px;
+  }
+`;
 
 const AppToolBar = () => {
   return (
     <ElementCard title="App tool bar">
-      <div></div>
+      <div>
+        <Navbar bg="light">
+          <Navbar.Brand>Light navbar</Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>Some content</Navbar.Text>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Navbar bg="dark">
+          <Navbar.Brand>Dark navbar</Navbar.Brand>
+        </Navbar>
+      </div>
       <Code>
         {`
+          <Navbar bg="light">
+            <Navbar.Brand>Light navbar</Navbar.Brand>
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>Some content</Navbar.Text>
+            </Navbar.Collapse>
+          </Navbar>
 
-        `}
-      </Code>
-    </ElementCard>
-  );
-};
-
-const Papers = () => {
-  return (
-    <ElementCard title="Paper">
-      <div></div>
-      <Code>
-        {`
-
+          <Navbar bg="dark">
+            <Navbar.Brand>Dark navbar</Navbar.Brand>
+          </Navbar>
         `}
       </Code>
     </ElementCard>
@@ -31,10 +49,45 @@ const Papers = () => {
 const Cards = () => {
   return (
     <ElementCard title="Card">
-      <div></div>
+      <SpacedChildren>
+        <SmallCard>
+          <Card.Header>Some header</Card.Header>
+          <Card.Img variant="top" src="http://via.placeholder.com/286x180" />
+          <Card.Body>
+            <Card.Title>Some card</Card.Title>
+            <Card.Text>Some content.</Card.Text>
+            <Card.Link href="#">Some link</Card.Link>
+          </Card.Body>
+        </SmallCard>
+
+        <SmallCard>
+          <Card.Img variant="top" src="http://via.placeholder.com/286x180" />
+          <Card.ImgOverlay>
+            <Card.Title>Some image overlay card</Card.Title>
+            <Card.Text>Some content.</Card.Text>
+            <Card.Link href="#">Some link</Card.Link>
+          </Card.ImgOverlay>
+        </SmallCard>
+      </SpacedChildren>
       <Code>
         {`
+          <Card>
+            <Card.Header>Some header</Card.Header>
+            <Card.Img variant="top" src="http://via.placeholder.com/286x180" />
+            <Card.Body>
+              <Card.Title>Some card</Card.Title>
+              <Card.Text>Some content.</Card.Text>
+              <Card.Link href="#">Some link</Card.Link>
+            </Card.Body>
+          </Card>
 
+          <Card>
+            <Card.Img variant="top" src="http://via.placeholder.com/286x180" />
+            <Card.ImgOverlay>
+              <Card.Title>Some image overlay card</Card.Title>
+              <Card.Text>Some content.</Card.Text>
+            </Card.ImgOverlay>
+          </Card>
         `}
       </Code>
     </ElementCard>
@@ -44,10 +97,46 @@ const Cards = () => {
 const Accordions = () => {
   return (
     <ElementCard title="Accordion">
-      <div></div>
+      <div>
+        <Accordion defaultActiveKey="0">
+          <Card>
+            <Accordion.Toggle as={Card.Header} eventKey="0">
+              Accordion 1
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>Some text</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card>
+            <Accordion.Toggle as={Card.Header} eventKey="1">
+              Accordion 2
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="1">
+              <Card.Body>Some text</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+      </div>
       <Code>
         {`
-
+          <Accordion defaultActiveKey="0">
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="0">
+                Accordion 1
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>Some text</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="1">
+                Accordion 2
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="1">
+                <Card.Body>Some text</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
         `}
       </Code>
     </ElementCard>
@@ -58,7 +147,6 @@ export const Surface = () => {
   return (
     <FlexWrapper>
       <AppToolBar />
-      <Papers />
       <Cards />
       <Accordions />
     </FlexWrapper>
